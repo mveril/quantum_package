b64655c6b729534452f6f0cc777f7318ef1983fd
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2018-10-30 11:59:38 +0100

    Fix indexes problems

--- plugins/CC/D_ijab.irp.f
+++ plugins/CC/D_ijab.irp.f
@@ -7,5 +7,5 @@ double precision function D_ijab(i,j,a,b)
   
   implicit none
   integer,intent(in) :: i,j,a,b
-  D_ijab=eigenvalues_fock_matrix_ao((a-n_spin_occ)/2+1)+eigenvalues_fock_matrix_ao((b-n_spin_occ)/2+1)-eigenvalues_fock_matrix_ao(i)-eigenvalues_fock_matrix_ao(j)
+  D_ijab=eigenvalues_fock_matrix_ao((a+1+n_spin_occ)/2)+eigenvalues_fock_matrix_ao((b+1+n_spin_occ)/2)-eigenvalues_fock_matrix_ao((i+1)/2)-eigenvalues_fock_matrix_ao((j+1)/2)
 end function
