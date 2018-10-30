51c8ce66718b97d655db4df81fd948fa32eb04f4
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2018-10-30 11:55:11 +0100

    Replace elec_num by n_spin_occ in D_ijab

--- plugins/CC/D_ijab.irp.f
+++ plugins/CC/D_ijab.irp.f
@@ -7,5 +7,5 @@ double precision function D_ijab(i,j,a,b)
   
   implicit none
   integer,intent(in) :: i,j,a,b
-  D_ijab=eigenvalues_fock_matrix_ao((a-elec_num)/2+1)+eigenvalues_fock_matrix_ao((b-elec_num)/2+1)-eigenvalues_fock_matrix_ao(i)-eigenvalues_fock_matrix_ao(j)
+  D_ijab=eigenvalues_fock_matrix_ao((a-n_spin_occ)/2+1)+eigenvalues_fock_matrix_ao((b-n_spin_occ)/2+1)-eigenvalues_fock_matrix_ao(i)-eigenvalues_fock_matrix_ao(j)
 end function
