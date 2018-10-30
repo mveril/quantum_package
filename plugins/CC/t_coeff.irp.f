9f25c5b161a1e67ac717ef74ce6e66c4de6588a9
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2018-10-30 09:08:56 +0100

    fix sign error

--- plugins/CC/t_coeff.irp.f
+++ plugins/CC/t_coeff.irp.f
@@ -14,7 +14,7 @@ BEGIN_PROVIDER [ double precision, t_coeff, (elec_num,elec_num,mo_tot_num*2-elec
     do j=1,elec_num
       do a=1,n_spin_virt
         do b=1,n_spin_virt
-        t_coeff(i,j,a,b)=ijab_antispinint(i,j,a,b)/D_ijab(i,j,a,b)
+        t_coeff(i,j,a,b)=ijab_antispinint(i,j,a,b)/(-D_ijab(i,j,a,b))
         enddo
       enddo
     enddo
