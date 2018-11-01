a824e4d20fa58f114aa2e43bc338ef673def8f57
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2018-11-01 15:06:35 +0100

    Fix a bug of spin in spinint function

--- plugins/CC/spinints.irp.f
+++ plugins/CC/spinints.irp.f
@@ -12,7 +12,7 @@ double precision function spinint(p,q,r,s)
   double precision :: get_mo_bielec_integral
   integer :: c,d
   spinint=0d0
-  if (mod(p,2) == mod(r,2)) then
+  if (mod(p,2) == mod(r,2) .and. mod(q,2)==mod(s,2))  then
     spinint = get_mo_bielec_integral((p+1)/2,(q+1)/2,(r+1)/2,(s+1)/2,mo_integrals_map)
   end if
 end function
